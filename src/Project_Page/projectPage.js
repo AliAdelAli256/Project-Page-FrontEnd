662b4759756b70ed16f60a0b35e519cfd21608d5
AliAdelAli256 <48568085+AliAdelAli256@users.noreply.github.com>
2021-05-26 00:20:10 +0200

    sidebar and file tree updated

--- src/Project_Page/projectPage.js
+++ src/Project_Page/projectPage.js
@@ -2,9 +2,71 @@ import React, { useState } from "react";
 import Editor, { useMonaco } from "@monaco-editor/react";
 import Terminal from "react-console-emulator";
 import ReactTooltip from "react-tooltip";
+import { Drawer } from '@material-ui/core';
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import TreeItem from "@material-ui/lab/TreeItem";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Collapse } from 'reactstrap';
 import "./projectPage.css";
+import "./Sidebar.css";
+
+const drawerWidth = 180;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 240,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+  drawer: {
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    position: 'absolute',
+    left: '42.8px',
+    top: '63px',
+    overflow: 'hidden',
+    height: '100%',
+  },
+}));
 
 function Ide(props) {
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+    if (open === false) {
+      document.getElementById('ide-mr').style.marginLeft = '180px';
+    }
+    else if (open === true) {
+      document.getElementById('ide-mr').style.marginLeft = '0px';
+    }
+
+  };
+
+  function MinusSquare(props) {
+    return (
+      <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+        {/* tslint:disable-next-line: max-line-length */}
+        <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
+      </SvgIcon>
+    );
+  }
+
+  function PlusSquare(props) {
+    return (
+      <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+        {/* tslint:disable-next-line: max-line-length */}
+        <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
+      </SvgIcon>
+    );
+  }
 
   const options = {
     language: "javascript",
@@ -26,8 +88,8 @@ function Ide(props) {
 
       <div class="btn-group-vertical justify-content-start bg-secondary" role="group" aria-label="Basic example" style={{ padding: '2px' }}>
 
-        <button data-tip data-for="fileSystem" type="button" class="btn btn-secondary mb-1 active" style={{ maxHeight: '40px', width: '40px', borderRadius: '5px' /*, boxShadow : '0px 0px 3px 3px gray'*/ }}><i class="fas fa-copy"></i></button>
-        <ReactTooltip id="fileSystem" place="right" effect="solid" backgroundColor="white" textColor="black">File System</ReactTooltip>
+        <button onClick={handleDrawerOpen} data-tip data-for="fileSystem" type="button" class="btn btn-secondary mb-1" style={{ maxHeight: '40px', width: '40px', borderRadius: '5px' /*, boxShadow : '0px 0px 3px 3px gray'*/ }}><i class="fas fa-copy"></i></button>
+        <ReactTooltip id="fileSystem" place="top" effect="solid" backgroundColor="white" textColor="black">File System</ReactTooltip>
 
         <button data-tip data-for="communication" type="button" class="btn btn-secondary mb-1 " data-toggle="tooltip" data-placement="right" title="Communication" style={{ maxHeight: '40px', width: '40px', borderRadius: '5px' }}><i class="fas fa-comments"></i></button>
         <ReactTooltip id="communication" place="right" effect="solid" backgroundColor="white" textColor="black">Communication</ReactTooltip>
@@ -39,6 +101,40 @@ function Ide(props) {
         <ReactTooltip id="review" place="right" effect="solid" backgroundColor="white" textColor="black">Review</ReactTooltip>
 
       </div>
+
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        backgroundColor="rgb(108, 117, 125);"
+      >
+        <div className="sidebar">
+          <TreeView
+            className={classes.root}
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ChevronRightIcon />}
+          >
+            <TreeItem nodeId="1" label="public">
+              <TreeItem nodeId="2" label="index.html" style={{color: '#000'}}/>
+              <TreeItem nodeId="3" label="manifest.json" />
+              <TreeItem nodeId="4" label="logo.png" />
+            </TreeItem>
+            <TreeItem nodeId="5" label="src">
+              <TreeItem nodeId="10" label="index.js" />
+              <TreeItem nodeId="6" label="component">
+                <TreeItem nodeId="7" label="tree">
+                  <TreeItem nodeId="8" label="tree-view.js" />
+                  <TreeItem nodeId="9" label="tree-item.js" />
+                </TreeItem>
+              </TreeItem>
+            </TreeItem>
+          </TreeView>
+        </div>
+      </Drawer>
 
       <div type="text" id="ide-mr" className="code">
         <Editor
@@ -52,11 +148,11 @@ function Ide(props) {
           errorText={errorText}
           ignoreCommandCase
           noEchoBack
-          promptLabel={">>>"}
+          promptLabel={">"}
           className="main-terminal"
           contentClassName="main-terminal-content"
-          promptLabelClassName="text-black"
-          inputClassName="text-black"
+          promptLabelClassName="text-white"
+          inputClassName="text-white"
         />
       </div>
 
